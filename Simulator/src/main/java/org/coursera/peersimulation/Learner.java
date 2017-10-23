2b88f7dc8527b7ff8a7c69d0ae12aaef7cdcd007
Dwarak <dwbs@paypal.com>
2017-10-22 21:31:38 -0700

    More changes

--- Simulator/src/main/java/org/coursera/peersimulation/Learner.java
+++ Simulator/src/main/java/org/coursera/peersimulation/Learner.java
@@ -2,97 +2,163 @@ package org.coursera.peersimulation;
 
 import java.util.List;
 
+
 public class Learner {
-	private int learnerId;
-	private int firstSubmissionStartTick;
-	private int firstSubmissionTrueScore;
-	private int reviewBias;
-	private State learnerState;
-	private List<Integer> submissionTicks;
-	private int numOfSubmissions;
-	private int numOfReviews;
-	private int currentScore;
-	private Submission submission;
-	public int getLearnerId() {
-		return learnerId;
-	}
-	public Learner setLearnerId(int learnerId) {
-		this.learnerId = learnerId;
-		return this;
-	}
-	public int getFirstSubmissionStartTick() {
-		return firstSubmissionStartTick;
-	}
-	public Learner setFirstSubmissionStartTick(int firstSubmissionStartTick) {
-		this.firstSubmissionStartTick = firstSubmissionStartTick;
-		return this;
-	}
-	public int getFirstSubmissionTrueScore() {
-		return firstSubmissionTrueScore;
-	}
-	public Learner setFirstSubmissionTrueScore(int firstSubmissionTrueScore) {
-		this.firstSubmissionTrueScore = firstSubmissionTrueScore;
-		return this;
-	}
-	public int getReviewBias() {
-		return reviewBias;
-	}
-	public Learner setReviewBias(int reviewBias) {
-		this.reviewBias = reviewBias;
-		return this;
-	}
-	public State getLearnerState() {
-		return learnerState;
-	}
-	public Learner setLearnerState(State learnerState) {
-		this.learnerState = learnerState;
-		return this;
-	}
-	public List<Integer> getSubmissionTicks() {
-		return submissionTicks;
-	}
-	public void setSubmissionTicks(List<Integer> submissionTicks) {
-		this.submissionTicks = submissionTicks;
-	}
-	public int getNumOfSubmissions() {
-		return numOfSubmissions;
-	}
-	public void setNumOfSubmissions(int numOfSubmissions) {
-		this.numOfSubmissions = numOfSubmissions;
-	}
-	public void startWork() {
-		this.learnerState=State.WORKING;
-		
-	}
-	public Submission submit(int tick) {
-	    if(submission==null){
-	        submission= new Submission();
-	    }
-		this.learnerState=State.WAITING_REVIEW;
-		this.submissionTicks.add(tick);
-		numOfSubmissions++;
-		return submission;
-		
-	}
-	public void startReviewing(Submission submission, int i) {
-		this.learnerState=State.REVIEWING;
-		numOfReviews++;
-		int learnerScore=submission.getLearner().getCurrentScore();
-		learnerScore+=this.reviewBias;
-		//submission
-		
-		
-	}
-	private int getCurrentScore() {
-		// TODO Auto-generated method stub
-		return currentScore;
-	}
-	public void endReview(int i) {
-		// TODO Auto-generated method stub
-		
-	}
-    
-	
-	
+    private static final int maxReviews = 3;
+    private static final int MAX_SUBMISSIONS = 0;
+    private int learnerId;
+    private int firstSubmissionStartTick;
+    private int firstSubmissionTrueScore;
+    private int reviewBias;
+    private State learnerState;
+    private List<Integer> submissionTicks;
+    private int numOfSubmissions;
+    private int numOfReviews;
+    private int currentScore;
+    private Submission submission;
+    private int nextActTime;
+    private int currentReviewScore;
+
+    private Submission reviewingSubmission;
+
+    public int getLearnerId() {
+        return learnerId;
+    }
+
+    public void actAtPulse(SimulationManager manager, int tick) {
+
+        if (tick == nextActTime) {
+            if (learnerState == State.NOT_STARTED) {
+                startWork();
+                
+                nextActTime = tick + 50;
+            }
+            if (learnerState == State.WORKING) {
+                manager.submit(submission);
+                nextActTime = tick + 1;
+            }
+            if (learnerState == State.WAITING_REVIEW) {
+                if (manager.isOutComeAvailable(this.submission)) {
+                    if (numOfSubmissions == MAX_SUBMISSIONS) {
+                        learnerState = State.FAILED;
+                    }
+                    else {
+                        int score = this.currentScore += 15;
+                        currentScore = Math.min(score, 100);
+                        startWork();
+                        nextActTime = tick + 50;
+                    }
+                }
+                else if (numOfReviews < maxReviews) {
+
+                    Submission submissionToReview = manager.getNextSubmissionToReview(this.getLearnerId());
+                    startReviewing(submissionToReview);
+                    nextActTime = tick + 20;
+                }
+            }
+            if (learnerState == State.REVIEWING) {
+                manager.turnInReview(reviewingSubmission, currentReviewScore);
+                nextActTime = tick + 1;
+            }
+        }
+
+    }
+
+    public int getNumOfReviews() {
+        return numOfReviews;
+    }
+
+    public void setNumOfReviews(int numOfReviews) {
+        this.numOfReviews = numOfReviews;
+    }
+
+    public Learner setLearnerId(int learnerId) {
+        this.learnerId = learnerId;
+        return this;
+    }
+
+    public int getFirstSubmissionStartTick() {
+        return firstSubmissionStartTick;
+    }
+
+    public Learner setFirstSubmissionStartTick(int firstSubmissionStartTick) {
+        this.firstSubmissionStartTick = firstSubmissionStartTick;
+        return this;
+    }
+
+    public int getFirstSubmissionTrueScore() {
+        return firstSubmissionTrueScore;
+    }
+
+    public Learner setFirstSubmissionTrueScore(int firstSubmissionTrueScore) {
+        this.firstSubmissionTrueScore = firstSubmissionTrueScore;
+        return this;
+    }
+
+    public int getReviewBias() {
+        return reviewBias;
+    }
+
+    public Learner setReviewBias(int reviewBias) {
+        this.reviewBias = reviewBias;
+        return this;
+    }
+
+    public State getLearnerState() {
+        return learnerState;
+    }
+
+    public Learner setLearnerState(State learnerState) {
+        this.learnerState = learnerState;
+        return this;
+    }
+
+    public List<Integer> getSubmissionTicks() {
+        return submissionTicks;
+    }
+
+    public void setSubmissionTicks(List<Integer> submissionTicks) {
+        this.submissionTicks = submissionTicks;
+    }
+
+    public int getNumOfSubmissions() {
+        return numOfSubmissions;
+    }
+
+    public void setNumOfSubmissions(int numOfSubmissions) {
+        this.numOfSubmissions = numOfSubmissions;
+    }
+
+    public void startWork() {
+        this.learnerState = State.WORKING;
+
+    }
+
+    public Submission submit(int tick) {
+        if (submission == null) {
+            submission = new Submission();
+        }
+        this.learnerState = State.WAITING_REVIEW;
+        this.submissionTicks.add(tick);
+       
+        numOfSubmissions++;
+        return submission;
+
+    }
+
+    public void startReviewing(Submission submissionToReview) {
+        this.learnerState = State.REVIEWING;
+        this.reviewingSubmission = submissionToReview;
+        numOfReviews++;
+        int learnerScore = submissionToReview.getLearner().getCurrentScore();
+        learnerScore += this.reviewBias;
+        currentReviewScore = Math.min(100, learnerScore);
+
+    }
+
+    private int getCurrentScore() {
+        // TODO Auto-generated method stub
+        return currentScore;
+    }
 
 }
--- Simulator/src/main/java/org/coursera/peersimulation/SimulationManager.java
+++ Simulator/src/main/java/org/coursera/peersimulation/SimulationManager.java
@@ -10,91 +10,115 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+
 public class SimulationManager {
-	int simulationDuration;
-	
-	List<Learner>[] learnerTimeline;
-	
-	List<Learner> learners= new ArrayList<>();
-	Queue<Learner> learnersReadyToReview= new LinkedList<>();
-	
-	@SuppressWarnings("unchecked")
-	public SimulationManager(int duration) {
-		this.simulationDuration=duration;
-		learnerTimeline=new List[duration+1];
-	}
+    int simulationDuration;
 
-	PriorityQueue<Submission> submissionQueue=new PriorityQueue<>(Comparator.comparing(Submission::getSubmissionTick).thenComparing(Submission::getLearnerId));
+    List<Learner>[] learnerTimeline;
 
-	public void registerLearner(Learner learner) {
-		learners.add(learner);
-		addLearnerToTimeLine(learner,learner.getFirstSubmissionStartTick());
-		
-	}
+    List<Learner> learners = new ArrayList<>();
+    Queue<Learner> learnersReadyToReview = new LinkedList<>();
 
-	
+    @SuppressWarnings("unchecked")
+    public SimulationManager(int duration) {
+        this.simulationDuration = duration;
+        learnerTimeline = new List[duration + 1];
+    }
 
-	public void simulatePeerReviews() {
-		for(int i=0; i<learnerTimeline.length; i++){
-			List<Learner> currentActive=learnerTimeline[i];
-			List<Learner> submittingUsers = currentActive.stream().filter(l->(l.getLearnerState()==State.NOT_STARTED|| l.getLearnerState()==State.FAILED)).collect(Collectors.toList());
-			for(Learner learner:submittingUsers){
-				learner.startWork();
-				addLearnerToTimeLine(learner,i+50);
-			}
-			
-			List<Learner> workingUsers = currentActive.stream().filter(l->l.getLearnerState()==State.WORKING).collect(Collectors.toList());
-			for(Learner learner:workingUsers){
-				learner.submit(i);
-				learnersReadyToReview.add(learner);
-				Submission submission = new Submission();
-				submissionQueue.add(submission);
-			}
-			
-			List<Learner> reviewSubmitters = currentActive.stream().filter(l->l.getLearnerState()==State.REVIEWING).collect(Collectors.toList());
-			for(Learner learner:reviewSubmitters){
-				learner.endReview(i);
-				
-			}
-			Learner reviewer=null;
-			while(!submissionQueue.isEmpty()){
-				reviewer=learnersReadyToReview.remove();
-				Submission current=submissionQueue.remove();
-				if(!current.getLearner().equals(reviewer) && !current.getReviewers().contains(reviewer)){
-				    
-				    current.getReviewers().add(reviewer);
-					reviewer.startReviewing(current.getLearner(),i);
-					addLearnerToTimeLine(reviewer, i+20);
-					if(!current.maxReviewersReached){
-					    submissionQueue.add(current);
-					}
-				}
-				else
-				{
-					if(!submissionQueue.isEmpty()){
-						Submission next=submissionQueue.remove();
-						reviewer.startReviewing(next.getLearner(), i);
-						
-					}
-					submissionQueue.add(current);
-					
-				}
-			}
-			
-		}
-		
-	}
+    PriorityQueue<Submission> submissionQueue = new PriorityQueue<>(
+            Comparator.comparing(Submission::getSubmissionTick).thenComparing(Submission::getLearnerId));
 
-	private void addLearnerToTimeLine(Learner learner,int time) {
-		if(time>simulationDuration) return;
-		List<Learner> learnersAtTime=learnerTimeline[time];
-		if(learnersAtTime==null){
-			learnersAtTime= new ArrayList<>();
-			
-		}
-		learnersAtTime.add(learner);
-		learnerTimeline[time]=learnersAtTime;
-		
-	}
+    public void registerLearner(Learner learner) {
+        learners.add(learner);
+        addLearnerToTimeLine(learner, learner.getFirstSubmissionStartTick());
+
+    }
+
+    public void simulatePeerReviews() {
+        for (int i = 0; i < learnerTimeline.length; i++) {
+            List<Learner> currentActive = learnerTimeline[i];
+            List<Learner> submittingUsers = currentActive.stream()
+                    .filter(l -> (l.getLearnerState() == State.NOT_STARTED || l.getLearnerState() == State.FAILED))
+                    .collect(Collectors.toList());
+            for (Learner learner : submittingUsers) {
+                learner.startWork();
+                addLearnerToTimeLine(learner, i + 50);
+            }
+
+            List<Learner> workingUsers = currentActive.stream().filter(l -> l.getLearnerState() == State.WORKING)
+                    .collect(Collectors.toList());
+            for (Learner learner : workingUsers) {
+                Submission submission =learner.submit(i);
+                learnersReadyToReview.add(learner);
+                
+                submissionQueue.add(submission);
+            }
+
+            List<Learner> reviewSubmitters = currentActive.stream().filter(l -> l.getLearnerState() == State.REVIEWING)
+                    .collect(Collectors.toList());
+            for (Learner learner : reviewSubmitters) {
+                learner.endReview(i);
+
+            }
+            Learner reviewer = null;
+            while (!submissionQueue.isEmpty()) {
+                reviewer = learnersReadyToReview.remove();
+                Submission current = submissionQueue.remove();
+                if (!current.getLearner().equals(reviewer) && !current.getReviewers().contains(reviewer)) {
+
+                    current.getReviewers().add(reviewer);
+                    reviewer.startReviewing(current, i);
+                    addLearnerToTimeLine(reviewer, i + 20);
+                    if (!current.maxReviewersReached) {
+                        submissionQueue.add(current);
+                    }
+                }
+                else {
+                    if (!submissionQueue.isEmpty()) {
+                        Submission next = submissionQueue.remove();
+                        reviewer.startReviewing(next.getLearner(), i);
+
+                    }
+                    submissionQueue.add(current);
+
+                }
+            }
+
+        }
+
+    }
+
+    private void addLearnerToTimeLine(Learner learner, int time) {
+        if (time > simulationDuration)
+            return;
+        List<Learner> learnersAtTime = learnerTimeline[time];
+        if (learnersAtTime == null) {
+            learnersAtTime = new ArrayList<>();
+
+        }
+        learnersAtTime.add(learner);
+        learnerTimeline[time] = learnersAtTime;
+
+    }
+
+    public void submit(Submission submission) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public Submission getNextSubmissionToReview(int learnerId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void turnInReview(Submission reviewingSubmission, int currentReviewScore) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public boolean isOutComeAvailable(Submission submission) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
 }
